7640c6f59798a49a4343b8ba06d9c197c6299594
ardaergin <ardergin@gmail.com>
2024-06-13 13:42:10 +0200

    config and .env update

--- app/config.py
+++ app/config.py
@@ -1,6 +1,15 @@
 import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
 
 class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY', 'your_secret_key')
-    SESSION_TYPE = 'filesystem'
-    SESSION_FILE_DIR = '/tmp/flask_session/'
+    SECRET_KEY = os.getenv('SECRET_KEY')
+    SESSION_TYPE = os.getenv('SESSION_TYPE')
+    SESSION_FILE_DIR = os.getenv('SESSION_FILE_DIR')
+    SESSION_PERMANENT = False
+    SESSION_USE_SIGNER = True
+    SESSION_COOKIE_HTTPONLY = True
+    SESSION_COOKIE_SECURE = os.getenv('SESSION_COOKIE_SECURE').lower() in ['true', '1', 't']
+    SESSION_COOKIE_SAMESITE = os.getenv('SESSION_COOKIE_SAMESITE')

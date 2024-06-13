fc1ca9bcc91c8fe2ac7fd7e061fb14db9a8f047b
ardaergin <ardergin@gmail.com>
2024-06-13 13:12:58 +0200

    file cleanup for signup (will develop later)

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,8 +1,6 @@
 from .main import bp as main_bp
 from .search import bp as search_bp
-from .sign_up import bp as sign_up_bp
 
 def register_blueprints(app):
     app.register_blueprint(main_bp)
     app.register_blueprint(search_bp)
-    app.register_blueprint(sign_up_bp)

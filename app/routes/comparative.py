a25553389e3c6a6783138ad4f810cae449381597
ardaergin <ardergin@gmail.com>
2025-04-09 12:44:38 +0200

    Small cleanup

--- app/routes/comparative.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, render_template, request, redirect, url_for
-
-
-bp = Blueprint('comparative', __name__)
-
-
-@bp.route('/comparative')
-def index():
-    return render_template('comparative.html')
